5e189d23e9ec5ee2cbcf321f7aad819dd3ed2758
Ariana <113695104+arianasmyr77@users.noreply.github.com>
2024-07-21 00:28:27 +0200

    chng

--- checks.c
+++ checks.c
@@ -23,3 +23,33 @@ void    check_arguments(t)
     else if (data->time_to_think <= 0)
         return (1) ;
 }
+
+int check_philo_death(t_data *data)
+{
+    int any_dead;
+
+    pthread_mutex_lock(&(data->any_dead_mutex));
+    any_dead = data->any_dead;
+    pthread_mutex_unlock(&(data->any_dead_mutex));
+    return (any_dead);
+}
+
+
+int check_die(t_data *data, t_philo *philo, int check_any)
+{
+    int time;
+
+    if (check_any)
+    {
+        if (check_philo_death(data))
+            return (1);
+    }
+
+    time = get_time(data);
+    if (time - philo->last_eat > data->time_to_die)
+    {
+        print_philo_msg(data, philo->id, DEAD);
+        return (1);
+    }
+    return (0);
+}
